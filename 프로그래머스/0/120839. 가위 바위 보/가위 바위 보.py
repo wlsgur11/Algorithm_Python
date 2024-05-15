bdea671ca052a7b545fd7f7ceae57d2625f87b2a
진혁 <87259233+wlsgur11@users.noreply.github.com>
2024-05-15 15:13:54 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,7 +1,3 @@
 def solution(rsp):
-    ans = ''
-    for i in rsp:
-        if i == '0': ans += '5'
-        if i == '2': ans += '0'
-        if i == '5': ans += '2'
-    return ans
+    A = {'0': '5', '2': '0', '5': '2'}
+    return "".join(A[i] for i in rsp)
