92a137e89376cfc8a40ba96a2c8c5a2feb6380a6
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2024-05-27 13:27:59 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,9 @@
+def solution(n):
+    result = []
+    for i in range(1, int(n**(1/2))+1):
+        if n%i==0:
+            result.append(i)
+            if i < n//i:
+                result.append(n//i)
+    result.sort()
+    return result 
