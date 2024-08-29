bb41ddf87f3951b21429390f0db23692bd232bcb
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2024-08-30 00:59:38 +0900

    [level 2] Title: 구명보트, Time: 8.85 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,12 @@
+def solution(people, limit):
+    cnt = 0
+    people.sort()
+    left, right = 0, len(people) - 1
+    
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1
+        right -= 1
+        cnt += 1
+        
+    return cnt
