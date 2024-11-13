dc664489cb9ff52ec4c4b6b29ec619f2da017614
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2024-11-13 15:53:55 +0900

    Time: 6 ms (93.34%), Space: 16.8 MB (77.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,6 +2,7 @@ from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
         A = Counter(s)
         B = Counter(t)
         return (A==B)
