e0de1813bad0a2bef31c251450ad8ec0efedd23f
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-01-15 12:52:41 +0900

    [Silver IV] Title: 듣보잡, Time: 188 ms, Memory: 120664 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,12 +1,11 @@
 N, M = map(int, input().split())
-dict = {}
-for i in range(N+M):
-    name = input()
-    if name not in dict: dict[name] = 1
-    else: dict[name] += 1
-ans = []
-for i in dict:
-    if dict[i] == 2: ans.append(i)
-print(len(ans))
-ans.sort()
-for i in ans: print(i)
+set1 = set()
+set2 = set()
+
+for i in range(N): set1.add(input())
+for i in range(M): set2.add(input())
+
+lst = list(set1 & set2)
+lst.sort()
+print(len(lst))
+for i in lst: print(i)
