85b4d2fb929b141af8156229b62b5c1e99e3b506
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-02-19 12:22:17 +0900

    [Bronze II] Title: 블랙잭, Time: 76 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,13 +1,11 @@
-# 풀이법1. 3중 for문 : 100개의 카드가 있으니까 O(N^3)해도 괜찮을 듯 하다.
-# 풀이법2. dfs는 잘 모르겠다.
 N, M = map(int, input().split())
-num_list = list(map(int, input().split()))
-ans = 0
+nums = list(map(int, input().split()))
 
-for i in range(len(num_list)-2):
-    for j in range(i+1, len(num_list)-1):
-        for k in range(j+1, len(num_list)):
-            now = num_list[i] + num_list[j] + num_list[k]
-            if (ans < now) and (now <= M):
-                ans = now
+ans = 0
+for i in range(N-2):
+    for j in range(i+1, N-1):
+        for k in range(j+1, N):
+            A = nums[i] + nums[j] + nums[k]
+            if A <= M and A > ans:
+                ans = A
 print(ans)
