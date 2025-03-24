cc3ec5762173e7c8f6e6cb8a11c23665536e5062
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-03-24 13:16:55 +0900

    [Gold IV] Title: 줄세우기, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.py
@@ -0,0 +1,11 @@
+N = int(input())
+arr = []
+for _ in range(N):
+    arr.append(int(input()))
+
+dp = [1] * N
+for i in range(N):
+    for j in range(i):
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(N - max(dp))
