06dc7ba02f5328b574450cda93d883aba65e4e6f
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-03-24 12:47:43 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 116 ms, Memory: 110068 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,30 +1,29 @@
 from collections import deque
 
 N, M = map(int, input().split())
-adj = {i: i for i in range(1, 101)}  # 기본적으로 자기 자신을 가리키도록 초기화
+adj = {i: i for i in range(1, 101)}
 
-for _ in range(N + M):
+for _ in range(N+M):
     x, y = map(int, input().split())
-    adj[x] = y  # 사다리와 뱀 적용
+    adj[x] = y
 
-q = deque([(1, 0)])  # 1번 칸에서 시작
+q = deque([(1, 0)])
 visited = [False] * 101
-visited[1] = True  # 시작 지점 방문 처리
-
+visited[1] = True
 while q:
     pos, step = q.popleft()
 
-    if pos == 100:  # 100번 칸 도착하면 종료
+    if pos == 100:
         print(step)
         break
 
-    for i in range(1, 7):  # 주사위 1~6까지 던지기
-        next_pos = pos + i
-        if next_pos > 100:
+    for i in range(1, 7):
+        next = pos + i
+        if next > 100:
             continue
 
-        next_pos = adj[next_pos]  # 사다리나 뱀 적용
+        next = adj[next]
 
-        if not visited[next_pos]:  # 방문하지 않은 경우만 이동
-            visited[next_pos] = True
-            q.append((next_pos, step + 1))
+        if not visited[next]:
+            visited[next] = True
+            q.append((next, step+1))
