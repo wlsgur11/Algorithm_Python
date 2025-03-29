893e9eef1829318f8f14b490f45329f010a8fd50
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-03-29 12:09:04 +0900

    [Gold V] Title: 적록색약, Time: 148 ms, Memory: 115152 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,57 +1,48 @@
 from collections import deque
 
-N = int(input())
-RGB = [list(map(str, input())) for _ in range(N)]
 
+def bfs(i, j, A):
+    q = deque([(i, j)])
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and A[nx][ny] == A[x][y]:
+                visited[nx][ny] = True
+                q.append((nx, ny)) 
+
+N = int(input())
+
+change_normal = {'R': 1, 'G': 2, 'B': 3}
+change_blind = {'R': 1, 'G': 1, 'B': 2}
+
+first = [list(map(str, input())) for _ in range(N)]
+
+normal = [[0] * N for _ in range(N)]
+blind = [[0] * N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        normal[i][j] = change_normal[first[i][j]]
+        blind[i][j] = change_blind[first[i][j]]
+
+visited = [[False] * N for _ in range(N)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-visited = [[False] * N for _ in range(N)]
-
-def bfs1(i, j):
-    q = deque([(i, j)])
-
-    while q:
-        x, y = q.popleft()
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
-            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and RGB[nx][ny] == RGB[x][y]:
-                visited[nx][ny] = True
-                q.append((nx, ny))
-
-def bfs2(i, j):
-    q = deque([(i, j)])
-
-    while q:
-        x, y = q.popleft()
-        visited[x][y] = True
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
-            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
-                if RGB[x][y] != 'B' and RGB[nx][ny] != 'B':
-                    visited[nx][ny] = True
-                    q.append((nx, ny))
-                elif RGB[x][y] == 'B' and RGB[nx][ny] == 'B':
-                    visited[nx][ny] = True
-                    q.append((nx, ny))
-
-ans1 = 0
-ans2 = 0
+ans = [0, 0]
 
 for i in range(N):
     for j in range(N):
         if not visited[i][j]:
-            bfs1(i, j)
-            ans1 += 1
+            bfs(i, j, normal)
+            ans[0] += 1
 
 visited = [[False] * N for _ in range(N)]
-
 for i in range(N):
     for j in range(N):
         if not visited[i][j]:
-            bfs2(i, j)
-            ans2 += 1
+            bfs(i, j, blind)
+            ans[1] += 1
 
-print(ans1, ans2)
+print(*ans)
