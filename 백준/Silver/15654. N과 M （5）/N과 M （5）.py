9d65e491876e6d49c875a0a3ad973e3de20a1513
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-04-01 14:30:16 +0900

    [Silver III] Title: N과 M (5), Time: 152 ms, Memory: 116496 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -2,8 +2,8 @@ from itertools import permutations
 
 N, M = map(int, input().split())
 nums = list(map(int, input().split()))
+nums.sort()
 
 A = list(permutations(nums, M))
-A.sort()
 
 for i in A: print(*i)
