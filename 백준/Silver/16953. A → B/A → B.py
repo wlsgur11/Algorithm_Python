04d8e483e6ddeced4f2abec6eeb79a90ae0f11b4
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-04-08 13:19:31 +0900

    [Silver II] Title: A → B, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,13 @@
+a,b = map(int,input().split())
+cnt = 1
+while(b!=a):
+    cnt+=1
+    temp = b
+    if b%10 == 1: b//=10
+    elif b%2 == 0: b//=2
+    
+    if temp == b:
+        print(-1)
+        break
+else:
+    print(cnt)
