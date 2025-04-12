5f03379f4c207c4146b89b492fec27fa9af7f078
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-04-12 13:34:01 +0900

    [level 2] Title: 소수 찾기, Time: 5.91 ms, Memory: 9.48 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,17 +1,19 @@
 from itertools import permutations
 
+def isPrime(n):
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False       
+    return True
+
 def solution(numbers):
-    ans = []
-    nums = [n for n in numbers]
-    per = []
-    for i in range(1, len(numbers) + 1):
-        per += list(permutations(nums, i))
-    new_nums = set([int(("").join(p)) for p in per]) - {0, 1}
+    A = set()
+    for i in range(1, len(numbers)+1):
+        for j in permutations(list(numbers), i):
+            A.add(int("".join(j)))
     
-    cnt = len(new_nums)
-    for n in new_nums:
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                cnt -= 1
-                break
+    cnt = 0
+    for i in A:
+        if 2 <= i and isPrime(i):
+            cnt += 1
     return cnt
