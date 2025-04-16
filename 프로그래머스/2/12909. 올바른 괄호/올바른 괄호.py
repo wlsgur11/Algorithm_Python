c00d478ad0d406086d4c123bef2e91d651030674
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-04-16 17:27:34 +0900

    [level 2] Title: 올바른 괄호, Time: 17.57 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,13 +1,8 @@
 def solution(s):
     A = 0
     for i in s:
-        if i == "(":
-            A += 1
-        elif i == ")":
-            A -= 1
-        if A < 0:
-            return False
-    if A == 0:
-        return True
-    else:
-        return False
+        if i == "(": A += 1
+        elif i == ")": A -= 1
+        if A == -1: return False
+    if A == 0: return True
+    else: return False
