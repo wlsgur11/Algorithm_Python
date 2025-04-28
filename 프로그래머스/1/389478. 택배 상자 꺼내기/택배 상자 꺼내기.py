788617dc2462032892ae704b2eefcbdbc37e82e8
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-04-28 16:38:15 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.12 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,26 +1,18 @@
 def solution(n, w, num):
-    if (n / w) + 0.5 > n//w:
-        A = n//w + 1
-    else:
-        A = n//w
+    if (n / w) + 0.5 > n//w: A = n//w + 1
+    else: A = n//w
     palette = [[0] * w for _ in range(A)]
     
-    for i in range(n):
-        palette[i//w][i%w] = i+1
-        
+    for i in range(n): palette[i//w][i%w] = i+1
     for i in range(A):
-        if i % 2 != 0:
-            palette[i].reverse()
+        if i % 2 != 0: palette[i].reverse()
     
     for i in range(A):
         for j in range(w):
             if palette[i][j] == num:
                 x = i
                 y = j
-    
     cnt = 0
     for i in range(A):
-        now = palette.pop()
-        if now[y] >= num:
-            cnt += 1
+        if palette.pop()[y] >= num: cnt += 1
     return cnt
