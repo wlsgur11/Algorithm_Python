98f5dec9224b4984aa67e48109cf7638e4564575
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-05-24 13:24:22 +0900

    [D2] Title: 간단한 369게임, Time: 59 ms, Memory: 50,176 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -1,16 +1,15 @@
-T = int(input())
+N = int(input())
 ans = []
-for i in range(1, T + 1):
-    c = str(i)
-    count_3 = c.count('3')
-    count_6 = c.count('6')
-    count_9 = c.count('9')
-    
-    if count_3 > 0 or count_6 > 0 or count_9 > 0:
-        # 3, 6, 9가 포함된 경우
-        result = '-' * (count_3 + count_6 + count_9)
-        ans.append(result)
-    else:
-        ans.append(c)
+
+for i in range(1, N+1):
+    num = str(i)
+
+    cnt_3 = num.count('3')
+    cnt_6 = num.count('6')
+    cnt_9 = num.count('9')
+
+    if cnt_3 > 0 or cnt_6 > 0 or cnt_9 > 0:
+        ans.append('-' * (cnt_3 + cnt_6 + cnt_9))
+    else: ans.append(i)
 
 print(*ans)
