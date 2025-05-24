5685dc2cf621240bb9c23fdbefb32b267a0e020d
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-05-24 11:30:49 +0900

    [D2] Title: 달팽이 숫자, Time: 59 ms, Memory: 53,760 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,28 +1,29 @@
 T = int(input())
 
-for t in range(1, T+1):
+for tc in range(1, T+1):
     N = int(input())
-    snail = [[0] * N for _ in range(N)]
-    
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
-    
+
+    nums = [[0] * N for _ in range(N)]
+
     x, y = 0, 0
     direction = 0
-    
-    for num in range(1, N*N + 1):
-        snail[x][y] = num
-        
+
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+
+    for i in range(1, N*N+1):
+        nums[x][y] = i
+
         nx = x + dx[direction]
         ny = y + dy[direction]
-        
-        if nx < 0 or nx >= N or ny < 0 or ny >= N or snail[nx][ny] != 0:
+
+        if nx < 0 or nx >= N or ny < 0 or ny >= N or nums[nx][ny] != 0:
             direction = (direction + 1) % 4
             nx = x + dx[direction]
             ny = y + dy[direction]
-        
+
         x, y = nx, ny
-    
-    print(f"#{t}")
-    for row in snail:
+
+    print(f"#{tc}")
+    for row in nums:
         print(*row)
