b80bd7df49da8cb7b6eb3c714628109b36794e3e
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-06-20 18:24:17 +0900

    [Silver II] Title: N과 M (9), Time: 124 ms, Memory: 114504 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,24 +1,8 @@
-import sys
-input = sys.stdin.readline
+from itertools import permutations
 
-n, m = map(int, input().split())
+N, M = map(int, input().split())
 nums = list(map(int, input().split()))
 nums.sort()
-res = list()
-visited = [False] * n
 
-def bt(depth):
-    if depth == m:
-        print(*res)
-        return
-    prev = 0
-    for i in range(n):
-        if not visited[i] and prev != nums[i]:
-            prev = nums[i]
-            visited[i] = True
-            res.append(prev)
-            bt(depth + 1)
-            visited[i] = False
-            res.pop()
-
-bt(0)
+for i in set(permutations(nums, M)):
+    print(*i)
