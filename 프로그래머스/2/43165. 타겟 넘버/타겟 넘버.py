8e85807080fb800ee3a7aa3a6ff4e269714c0c60
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2025-12-28 17:04:37 +0900

    [level 2] Title: 타겟 넘버, Time: 850.25 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,18 +2,15 @@ from collections import deque
 
 def solution(numbers, target):    
     cnt = 0
-
+    
     q = deque([(0, numbers[0]), (0, -numbers[0])])
-
     while q:
         i, total = q.popleft()
-
-        if total == target and i == len(numbers) - 1:
+        
+        if i == len(numbers)-1 and total == target:
             cnt += 1
-
         if i+1 < len(numbers):
             q.append((i+1, total + numbers[i+1]))
             q.append((i+1, total - numbers[i+1]))
-
-    return cnt
         
+    return cnt
