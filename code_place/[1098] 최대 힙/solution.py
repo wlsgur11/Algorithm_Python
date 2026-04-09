498aca8e48e2350e84e32cc8db0b1ccca5582c59
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2026-04-10 05:25:30 +0900

    2026년 04월 10일 05:25:28

---
+++ code_place/[1098] 최대 힙/solution.py
@@ -0,0 +1,14 @@
+import heapq
+​
+N = int(input())
+heap = []
+​
+for _ in range(N):
+    A = int(input())
+    if A == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap)[1])
+    else:
+        heapq.heappush(heap, (-A, A))
