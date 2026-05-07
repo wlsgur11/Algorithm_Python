7b75eed743e0e18a3d75e81b9ffd1c6061b8c3ba
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2026-05-07 22:49:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 41.53 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,18 +2,20 @@ import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        // 1. 해십ㅁ맵을 만든다
+        // 해시맵 생성 participant로
         HashMap<String, Integer> map = new HashMap<>();
         for (String player: participant)
             map.put(player, map.getOrDefault(player, 0) + 1);
         
-        // 2. 해시맵에 participant를 뺀다
+        // 해시맵 수정 completion으로 - 1
         for (String player: completion)
             map.put(player, map.get(player) - 1);
-        // 4. value가 0이 아닌 사람을 찾는다
-        for (String key: map.keySet())
-            if (map.get(key) != 0)
+        
+        // 0이 아닌 keySet에서 찾기
+        for (String key: map.keySet()) 
+            if (map.get(key) != 0) 
                 return key;
         return "";
+
     }
 }
