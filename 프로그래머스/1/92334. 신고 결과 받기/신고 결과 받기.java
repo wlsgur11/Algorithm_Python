81b65b5110741509ebd5bc66baf2e3f8b67d716a
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2026-05-08 11:52:45 +0900

    [level 1] Title: 신고 결과 받기, Time: 129.85 ms, Memory: 171 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        // 중복 신고 제거하기
+        HashSet<String> reportSet = new HashSet<>(Arrays.asList(report));
+        
+        // 신고당한 횟수 기록
+        HashMap<String, Integer> reportCountMap = new HashMap<>();
+        for (String rep: reportSet){
+            String reported = rep.split(" ")[1];
+            reportCountMap.put(reported, reportCountMap.getOrDefault(reported, 0) + 1);
+        }    
+        
+        // 내가 신고한 사람 중 정지 된 사람이 몇명인지
+        HashMap<String, Integer> mailCountMap = new HashMap<>();
+        
+        for (String rep: reportSet){
+            String[] name = rep.split(" ");
+            String reporter = name[0];
+            String reported = name[1];
+            
+            if (reportCountMap.get(reported) >= k){
+                mailCountMap.put(reporter, mailCountMap.getOrDefault(reporter, 0) + 1);
+            }
+        }
+        
+        // 결과 배열 만들기
+        int[] answer = new int[id_list.length];
+        for (int i = 0; i < id_list.length; i++){
+            answer[i] = mailCountMap.getOrDefault(id_list[i], 0);
+        }
+        return answer;
+    }
+}
