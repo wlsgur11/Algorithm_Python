93bb3144e5378733bf318849527cae75f79818b2
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2026-05-09 00:53:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 38.17 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 41.53 ms
+메모리: 102 MB, 시간: 38.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 07일 22:49:45
+2026년 05월 09일 00:53:37
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,21 +1,22 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        // 해시맵 생성 participant로
         HashMap<String, Integer> map = new HashMap<>();
-        for (String player: participant)
+        
+        for (String player: participant){
             map.put(player, map.getOrDefault(player, 0) + 1);
+        }
         
-        // 해시맵 수정 completion으로 - 1
-        for (String player: completion)
+        for (String player: completion){
             map.put(player, map.get(player) - 1);
+        }
         
-        // 0이 아닌 keySet에서 찾기
-        for (String key: map.keySet()) 
-            if (map.get(key) != 0) 
+        for (String key: map.keySet()){
+            if (map.get(key) == 1){
                 return key;
+            }
+        }
         return "";
-
     }
 }
