fe38abda9cd886980bf83ad427269447186a6a54
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2026-06-21 14:04:25 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -2,9 +2,13 @@ class Solution {
     public int solution(int n) {
         int ans = 0;
         
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i*i <= n; i++) {
             if (n % i == 0) {
                 ans += i;
+                
+                if (i != n /i) {
+                    ans +=  n/i;
+                }
             }
         }
         return ans;
