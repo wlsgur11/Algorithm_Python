07fa7f868120b945931f02cfdbad65943019bebc
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2026-06-24 09:44:12 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.05 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 0.04 ms
+메모리: 75.9 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 22일 21:06:56
+2026년 06월 24일 09:44:10
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -3,16 +3,16 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int idx = 0;
         
-        for (char c: s.toCharArray()) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
             if (c == ' ') {
-                sb.append(c);
+                sb.append(' '); 
                 idx = 0;
-            } else {
-                if (idx % 2 == 0) {
-                    sb.append(Character.toUpperCase(c));
-                } else {
-                    sb.append(Character.toLowerCase(c));
-                }
+            } 
+            else {
+                if (idx % 2 == 0) sb.append(Character.toUpperCase(c));
+                else sb.append(Character.toLowerCase(c));
                 idx++;
             }
         }
