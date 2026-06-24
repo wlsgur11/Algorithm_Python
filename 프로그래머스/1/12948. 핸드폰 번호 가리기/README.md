b7796fb322ce599d85d6f44835a7032e8554bf0f
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2026-06-24 09:38:16 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/README.md
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 핸드폰 번호 가리기 - 12948 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948) 
 
 ### 성능 요약
 
-메모리: 74.1 MB, 시간: 2.02 ms
+메모리: 74.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 22일 08:27:25
+2026년 06월 24일 09:38:14
 
 ### 문제 설명
 
--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,9 +1,9 @@
     class Solution {
         public String solution(String phone_number) {
-            String ans = "";
-            for (int i = 0; i < phone_number.length()-4; i++) {
-                ans += "*";
-            }
-            return ans + phone_number.substring(phone_number.length()-4);
+            StringBuilder sb = new StringBuilder();
+            int n = phone_number.length();
+            for (int i = 0; i < n-4; i++) sb.append('*');
+            sb.append(phone_number.substring(n-4));
+            return sb.toString();
         }
     }
