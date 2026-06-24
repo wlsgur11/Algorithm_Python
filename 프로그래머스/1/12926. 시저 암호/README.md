f0dc06d289041d6c6513c60601fd7a29a97c66f9
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2026-06-24 09:49:06 +0900

    [level 1] Title: 시저 암호, Time: 0.66 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/README.md
+++ 프로그래머스/1/12926. 시저 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 시저 암호 - 12926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 0.66 ms
+메모리: 75.3 MB, 시간: 0.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 23일 08:54:20
+2026년 06월 24일 09:49:04
 
 ### 문제 설명
 
--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,12 +1,13 @@
 class Solution {
     public String solution(String s, int n) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i< s.length(); i++) {
             char c = s.charAt(i);
-            
             if (c == ' ') sb.append(' ');
-            else if (Character.isUpperCase(c)) sb.append((char) ('A' + (c - 'A' + n) % 26));
-            else sb.append((char) ('a' + (c - 'a' + n) % 26));
+            else {
+                if (Character.isUpperCase(c)) sb.append((char) ('A' + (c - 'A' + n) % 26));
+                else sb.append((char) ('a' + (c - 'a' + n) % 26));   
+            }
         }
         return sb.toString();
     }
