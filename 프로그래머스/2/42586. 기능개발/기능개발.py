f34b505678296ade29a71792cb85d473a90b0f0a
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2026-07-01 08:05:57 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,11 +1,25 @@
+import math
+
 def solution(progresses, speeds):
-    arr = [((100-a) + b - 1) // b for a, b in zip(progresses, speeds)]
+    stack = []
+    
+    for i, j in zip(progresses, speeds):
+        stack.append(math.ceil((100-i) / j))
+        
+    if len(stack) == 1:
+        return stack
+    
     ans = []
-    front = 0
-    print(arr)
-    for i in range(len(arr)):
-        if arr[i] > arr[front]:
-            ans.append(i-front)
-            front = i
-    ans.append(len(arr) - front)
+    cur = stack[0]
+    cnt = 1
+    
+    for i in range(1, len(stack)):
+        if stack[i] <= cur:
+            cnt += 1
+        else:
+            ans.append(cnt)
+            cur = stack[i]
+            cnt = 1
+    ans.append(cnt)
     return ans
+    
