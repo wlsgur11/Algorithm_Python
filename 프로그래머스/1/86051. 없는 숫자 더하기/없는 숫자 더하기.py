be442d46d5abf0ad0193eaa498fb9dd6097a5eb0
문진혁 <87259233+wlsgur11@users.noreply.github.com>
2026-07-15 11:29:20 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,7 +1,4 @@
 def solution(numbers):
-    a = [i for i in range(10)]
-    b = []
-    for i in range(10):
-        if a[i] not in numbers:
-            b.append(a[i])
-    return sum(b)
+    num = set(numbers)
+    nums = set([i for i in range(10)])
+    return sum(nums - num)
